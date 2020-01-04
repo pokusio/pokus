c4a4182ebdbbd354ff4538a1bd1a6496857d0654
pegasus.devops <jibl@poste-devops-jbl-16gbram.pegasus.io>
2020-01-05 00:32:21 +0100

    commit initial: premiers éléments de code pour faire un rest endpoint traitant le CRUD sur un fichier particulier, dont le chemin est spécifié en paramètre

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
-import { RegisterRoutes } from './routes/routes';  // cet import permet l'enrollmeent des routes générées par tsoa
+import { RegisterRoutes } from './routes/routes';  // cet import permet l'enrôlement des routes générées par tsoa
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+RegisterRoutes(app);  // permet l'enrôlement des routes générées par tsoa
 
 app.listen(port, () => console.log(`Server started listening to port ${port}`));
