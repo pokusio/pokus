b3b6f52cb1c6c1a35683c02625119fffbcdd0f96
pegasus.devops <jibl@poste-devops-jbl-16gbram.pegasus.io>
2020-01-05 01:52:08 +0100

    Ajout de la dépendance multer pour gérer le download upload fichiers

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,40 +1,26 @@
-import { Post, Request, Response, Route, SuccessResponse, Tags } from 'tsoa'
-
-import * as express from 'express'
-import * as multer from 'multer'
-import { StatusError } from '../models/StatusError'
+import { Get, Route, Security, Response } from 'tsoa';
+import * as express from 'express';
+import * as multer from 'multer';
 
 @Route('files')
 export class FilesController {
 
-  @Tags('File')
-  @SuccessResponse('204', 'successful')
-  @Response('400', 'invalid file supplied')
   @Post('uploadFile')
-  public async uploadImageFile (@Request() request: express.Request): Promise {
-    await this.handleFile(request, 'imageFile', 'zip', 'tmp', 'img.zip')
-    // file will be in request.imageFile, it is a buffer
+  public async uploadFile(@Request() request: express.Request): Promise<any> {
+    await this.handleFile(request);
+    // file will be in request.randomFileIsHere, it is a buffer
+    return {};
   }
 
-  private async handleFile (request: express.Request,
-                            requestField: string,
-                            filterExt: string,
-                            storePath: string,
-                            fileName?: string): Promise {
-    const fileFilter = (req: Express.Request, file: Express.Multer.File,
-                        callback: (error: Error | null, acceptFile: boolean) => void) => {
-      // accept image only
-      if (!file.originalname.match(`^(.*\\.((${filterExt})$))
-      console.log("Nom fichier : [" + file.originalname + "]" )
-    }
-  }
-
-  @Tags('File')
-  @SuccessResponse('200', 'successful')
-  // @Produces('application/zip, application/octet-stream') // hopefully this will work in the near future
-  @Get('download/image')
-  public async downloadImageFile (): Promise {
-    const absPath = await service.generateImageFile()
-    return FileResult.newInstance({ path: absPath })
+  private handleFile(request: express.Request): Promise<any> {
+    const multerSingle = multer().single('randomFileIsHere');
+    return new Promise((resolve, reject) => {
+      multerSingle(request, undefined, async (error) => {
+        if (error) {
+          reject(error);
+        }
+        resolve();
+      });
+    });
   }
 }
