cfca945a33e2684723b1c44cfe1a915efaf7eeb2
pegasus.devops <jibl@poste-devops-jbl-16gbram.pegasus.io>
2020-01-05 03:14:08 +0100

    Ajout de la dépendance multer pour gérer le download upload fichiers

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -1,6 +1,8 @@
 import { Post, Get, Route, Security, Response, Request } from 'tsoa';
 import * as express from 'express';
 import * as multer from 'multer';
+import * as path from 'path';
+
 
 @Route('files')
 export class FilesController {
@@ -11,13 +13,24 @@ export class FilesController {
   public async uploadFile(@Request() request: express.Request): Promise<any> {
     await this.handleFile(request);
     // file will be in request.randomFileIsHere, it is a buffer
-    console.log ('J ai invoqué le endpoint upload file');
-    return { msg: 'J ai invoqué le endpoint upload file'};
+    console.log ('J ai invoquÃ© le endpoint upload file');
+    return { msg: 'J ai invoquÃ© le endpoint upload file'};
   }
 
   private handleFile(request: express.Request): Promise<any> {
+  const pokusStorageOnDisk = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'worspace/pokus');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+ });
+
     const multerSingle = multer({ storage: pokusStorageOnDisk}).single('randomFileIsHere');
-    console.log(" TEST DU MULTER SINGLE TRAITé : [" + multerSingle + "]")
+    console.log(" TEST DU MULTER SINGLE TRAITÃ© : [" + multerSingle + "]")
     return new Promise((resolve, reject) => {
       multerSingle(request, undefined, async (error) => {
         if (error) {
